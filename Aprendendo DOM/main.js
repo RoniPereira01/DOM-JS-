30863afccd2241fccd47a4268b1c3a81e90c87b3
Ronilson Pereira <129990000+RoniPereira01@users.noreply.github.com>
2024-02-05 12:25:51 -0300

    Add files via upload

---
+++ Aprendendo DOM/main.js
@@ -0,0 +1,21 @@
+function show(){
+    const contactList= document.getElementById('contact-list')
+    console.log (contactList)
+
+
+    const listElements= document.getElementsByTagName('li')
+    console.log(listElements)
+
+    const contactIpunts = document.getElementsByClassName('contact-input')
+    console.log(contactIpunts)
+
+
+    const constacts = document.querySelectorAll('#contact-list > li > label ')
+    console.log(constacts)
+
+    const contact1 = document.getElementsByName('contact1')
+    console.log(contact1)
+
+    const firstContact= Document.queryselector('#contact-list > li > label')
+    console.log(firstContact)
+}
